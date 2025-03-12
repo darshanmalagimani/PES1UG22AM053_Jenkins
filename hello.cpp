0f374132600f26920a3519cb77eb4420d920a6a1
Darshan Malagimani <darshan.malagimani@outlook.com>
2025-03-12 20:03:51 +0530

    Updated the hello.cpp File

--- hello.cpp
+++ hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   cout << "My Name is Darshan S M, I am currently studying Sem : 6 \n";
+  cout << "Jenkins & Github Integration was amazing!";
   return 0;
 }
